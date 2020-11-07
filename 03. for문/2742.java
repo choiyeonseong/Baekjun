215533d4b1ec2aef1566a10c5e90c57fa092c25c
choiyeonseong <48269478+choiyeonseong@users.noreply.github.com>
2020-11-08 03:23:45 +0900

    Create 2742.java

---
+++ 03. for문/2742.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+		br.close();
+		for (int i = T; i > 0; i--) {
+
+			bw.write(i + "\n");
+		}
+		bw.flush();
+
+		bw.close();
+
+	}
+}
